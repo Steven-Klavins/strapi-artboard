824aaad8b5a5593486c3a99391c0440d72e4c5c4
Steven_Klavins <stevenklavins94@gmail.com>
2021-03-24 21:27:25 +0000

    chore: imported axios and made local request

--- strapi-artboard-front-end/src/App.js
+++ strapi-artboard-front-end/src/App.js
@@ -1,12 +1,37 @@
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
-import './App.css';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className = "header">
-      <h1>Art Board</h1>
-    </div>
-  );
+  state = {
+    pageTitle: ""
+  } 
+
+  async componentDidMount() {
+
+    const titleRes = await axios({
+      method: "GET",
+      url: "http://localhost:1337/titles"
+    });
+
+    const pageTitle = titleRes.data
+    this.setState({pageTitle})
+  }
+
+ 
+
+  render() {
+    
+    const {pageTitle} = this.state
+    console.log(pageTitle)
+    return (
+      <div className="header">
+        <h1>Art Board</h1>
+        {pageTitle.toString}
+      </div>
+    );
+  }
 }
 
 export default App;
